febe66687966eecd12ec7648134f81748b60ecac
HD1G <rhashier@oh.rr.com>
2020-07-07 18:38:18 -0400

    Employee Directory

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -48,9 +48,19 @@ export default class DataArea extends Component {
           return a[heading] - b[heading];
         }
       } else {
-        // else statement to handle if this.state.order === descend
+          console.log("Descending condition", a[heading])
+          if (a[heading] === undefined) {
+            console.log("Returning 1")
+            return 1;
+          } else if (b[heading] === undefined) {
+            return -1;
+          }
+          else if (heading === "name") {
+            return b[heading].first.localeCompare(a[heading].first);
+          } else {
+            return b[heading] - a[heading];
+          }
       }
-
     }
     const sortedUsers = this.state.filteredUsers.sort(compareFnc);
     this.setState({ filteredUsers: sortedUsers });
@@ -59,21 +69,14 @@ export default class DataArea extends Component {
   handleSearchChange = event => {
     event.preventDefault()
     console.log(event.target.value);
-    const filter = this.state.input;
-    const filteredList = this.state.users.filter(item => 
+    const filter = event.target.value;
+    const filteredList = this.state.users.filter(item => {
       // merge data together, then see if user input is anywhere inside
-      (item.name.first + " " + item.name.last === filter)
-      )
-
-    if (filteredList.length===0) {
-      alert("No results found.")
-      setTimeout (function () {window.location.reload(false)}, 1000);
-    }  
-
-    
+      let values = Object.values(item).join("").toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
     this.setState({ filteredUsers: filteredList });
   }
-
   componentDidMount() {
     API.getUsers().then(results => {
       this.setState({users: results.data.results, filteredUsers: results.data.results})
